280ef4b0d6276296517b34e50a62cf33dfdbc78b
ku20298 <31235499+ku20298@users.noreply.github.com>
2019-10-24 20:25:31 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,29 @@
 // ServiceWorker処理：https://developers.google.com/web/fundamentals/primers/service-workers/?hl=ja
 
 // キャッシュ名とキャッシュファイルの指定
-// var CACHE_NAME = 'rpgtest-caches2';
-// var urlsToCache = [
-// 	'/rpgtest/',
-// 	'/rpgtest/main.js'
-// ];
+var CACHE_NAME = 'rpgtest-caches2';
+var urlsToCache = [
 
-// // インストール処理
-// self.addEventListener('install', function(event) {
-// 	event.waitUntil(
-// 		caches
-// 			.open(CACHE_NAME)
-// 			.then(function(cache) {
-// 				return cache.addAll(urlsToCache);
-// 			})
-// 	);
-// });
+];
 
-// // リソースフェッチ時のキャッシュロード処理
-// self.addEventListener('fetch', function(event) {
-// 	event.respondWith(
-// 		caches
-// 			.match(event.request)
-// 			.then(function(response) {
-// 				return response ? response : fetch(event.request);
-// 			})
-// 	);
-// });
+// インストール処理
+self.addEventListener('install', function(event) {
+	event.waitUntil(
+		caches
+			.open(CACHE_NAME)
+			.then(function(cache) {
+				return cache.addAll(urlsToCache);
+			})
+	);
+});
+
+// リソースフェッチ時のキャッシュロード処理
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		caches
+			.match(event.request)
+			.then(function(response) {
+				return response ? response : fetch(event.request);
+			})
+	);
+});
